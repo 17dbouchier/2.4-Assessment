f1e291435359ae661bd4a070ff99d0128d61035d
Dillon Bouchier <dillonbouchier@Johannes-Ewan-Mackays-Mackaybook-Pro.local>
2020-05-05 10:11:29 +1200

    First attempt to create a video in landing page. Mostly successful.

---
+++ app.js
@@ -0,0 +1,9 @@
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+  }
+}
