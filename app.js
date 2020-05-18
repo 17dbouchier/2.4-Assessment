14109dabb93aa4a7d524d9161f6f8469c65e22d0
Dillon Bouchier <dillonbouchier@Anonymus-Prime.local>
2020-05-18 20:58:54 +1200

    First working prototype of hamburger menu w/ animated button.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-AOS.init();
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+    document.getElementById("mySidenav").style.width = "20%";
+  }
+  else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+    document.getElementById("mySidenav").style.width = "0";
+  }
+});
