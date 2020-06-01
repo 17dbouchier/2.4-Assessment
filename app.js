8a19aa1324e44da2abdd566d17159180f1b6b9b4
Dillon Bouchier <dillonbouchier@Johannes-Ewan-Mackays-Mackaybook-Pro.local>
2020-06-02 09:40:41 +1200

    Improved responsiveness of nav sidebar

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 document.getElementById("sidenav-links").style.opacity= "0";
+document.getElementById("aiHeader").style.opacity= "100%";
 
 menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
-    document.getElementById("mySidenav").style.width = "20%";
     document.getElementById("sidenav-links").style.opacity = "100%";
+    if (x.matches) {
+      document.getElementById("mySidenav").style.width = "100%";
+      document.getElementById("aiHeader").style.opacity = "0";
+    } else {
+      document.getElementById("mySidenav").style.width = "20%";
+      document.getElementById("aiHeader").style.opacity= "100%";
+    }
   }
   else {
     menuBtn.classList.remove('open');
     menuOpen = false;
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("sidenav-links").style.opacity= "0";
+    document.getElementById("aiHeader").style.opacity= "100%";
   }
 });
+
+window.addEventListener('resize', () => {
+  if(!menuOpen) {
+
+  } else {
+    if (x.matches) {
+      document.getElementById("mySidenav").style.width = "100%";
+      document.getElementById("aiHeader").style.opacity = "0";
+    } else {
+      document.getElementById("mySidenav").style.width = "20%";
+      document.getElementById("aiHeader").style.opacity= "100%";
+    }
+  }
+});
+
+var x = window.matchMedia("(max-width: 1055px)")
+mobileDeviceFunction(x) // Call listener function at run time
+x.addListener(mobileDeviceFunction) // Attach listener function on state changes
